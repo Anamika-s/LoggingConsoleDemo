24606cda319dd60875a6df6d24d4581f63871d98
Anamika <anamika.sawhney22@gmail.com>
2021-03-17 15:06:40 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static log4net.ILog logger = log4net.LogManager.GetLogger(typeof(Program));  //Declaring Log4Net  
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            
+            logger.Warn("Logging is started");
+            int x, y;
+            try
+            {
+                Console.WriteLine("Enter x");
+                x = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Enter y");
+                y = Convert.ToByte(Console.ReadLine());
+                int z = 0;
+                z = x % y;
+            }
+            catch(Exception ex)
+            {
+                logger.Fatal(ex.Message);
+            }
+           
+
+        }
+    }
+}
---
+++ LoggingConsoleDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net.Config;
+using LoggingDemo.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingConsoleDemo
+{
+    class Program
+    {
+       private static log4net.ILog logger = log4net.LogManager.GetLogger(typeof(Program));  //Declaring Log4Net  
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            logger.Error("Its an error");
+            logger.Warn("Warning");
+            Log.Error("Login-page started...");
+
+        }
+    }
+}
